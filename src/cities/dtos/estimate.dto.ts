df11d953ea412cce6217005080e9dc308d211941
Igor Silva <igor-gab@hotmail.com>
2024-11-03 23:52:34 -0300

    fix: param validation

--- src/cities/dtos/estimate.dto.ts
+++ src/cities/dtos/estimate.dto.ts
@@ -1,4 +1,7 @@
+import { IsNotEmpty } from "class-validator";
+
 export class EstimateDto {
+    @IsNotEmpty({message: "O nome do arquivo é parâmetro obrigatório"})
     pollName: string;
 
     constructor(pollName: string){
